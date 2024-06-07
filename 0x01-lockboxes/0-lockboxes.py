a4abcc7f98d6bc53f4424b02dedcb7f030641084
Shaker Sharabi <sheko100@icloud.com>
2024-06-07 05:08:31 +0300

    Add 0-lockboxes.py file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines canUnlockAll function
+"""
+
+
+def canUnlockAll(boxes):
+    """Checks if all boxes can be opened or not"""
+    state = True
+    for box in boxes:
+        for key in box:
+            pass
+
+    return state
