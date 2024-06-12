a180c410137361ffec172e5585c23f544e1e0c67
Shaker Sharabi <sheko100@icloud.com>
2024-06-12 09:02:25 +0300

    Add 0-lockboxes.py file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,8 +6,18 @@
 def canUnlockAll(boxes):
     """Checks if all boxes can be opened or not"""
     state = True
+    keys = [0]
+
     for box in boxes:
         for key in box:
-            pass
+            if key not in keys and key != boxes.index(box):
+                keys.append(key)
+
+    print(keys)
+
+    for i in range(len(boxes)):
+        if i not in keys:
+            state = False
+            break
 
     return state
