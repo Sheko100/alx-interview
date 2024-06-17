9b41e7d08ddae4c6aab06e2b9e0f939b6541e9ed
Shaker Sharabi <sheko100@icloud.com>
2024-06-17 05:11:50 +0300

    Add 0-minoperations.py file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,13 +5,27 @@
 
 def minOperations(n):
     """Calculates the minimum number of using the copy all and paste
-    operations so a file can have n number of H in the file
+    operations so a file can have n number of H character in the file
 
     Returns:
         number of operations
         or 0 if n < 2
     """
-    min_ops = 0
+    min_ops = 2
+    chars_num = 2
+    copied = 1
 
     if n < 2:
-        return min_ops
+        return 0
+
+    while chars_num < n:
+        rest = n % chars_num
+        if rest % chars_num == 0:
+            copied = chars_num
+            min_ops += 2
+            chars_num += copied
+        else:
+            chars_num += copied
+            min_ops += 1
+
+    return min_ops
