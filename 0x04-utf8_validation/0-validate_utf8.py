ccc30a270d1f32e29a0ed1cc64e0ea6f92552d6f
Shaker Sharabi <sheko100@icloud.com>
2024-06-28 05:40:36 +0300

    Add 0-validate_utf8.py file

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that defines function validUTF8
+"""
+
+
+def validUTF8(data):
+    """Checks if data is valid utf-8
+    """
+    is_valid = True
+
+    try:
+        string = bytes(data)
+        print(string)
+    except ValueError:
+        is_valid = False
+
+    return is_valid
