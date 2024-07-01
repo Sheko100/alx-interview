9729d26f5efb5a4c3a564523393f3fa336457598
Shaker Sharabi <sheko100@icloud.com>
2024-07-01 04:20:13 +0300

    Add 0-validate_utf8.py file

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,11 +6,19 @@
 def validUTF8(data):
     """Checks if data is valid utf-8
     """
-    is_valid = True
-
-    try:
-        string = bytes(data)
-    except ValueError:
-        is_valid = False
-
-    return is_valid
+    expected_continuations = 0
+    for byte in data:
+        if expected_continuations == 0:
+            if (byte >> 5) == 0b110:
+                expected_continuations = 1
+            elif (byte >> 4) == 0b1110:
+                expected_continuations = 2
+            elif (byte >> 3) == 0b11110:
+                expected_continuations = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            expected_continuations -= 1
+    return expected_continuations == 0
