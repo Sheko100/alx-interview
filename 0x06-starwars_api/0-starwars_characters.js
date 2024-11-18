cb5f720f12199cccffa03d03be1571182bb923fb
Shaker Sharabi <shakersharabi100@gmail.com>
2024-11-18 04:51:42 +0200

    Add 0-starwars_characters.js file

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const request = require('request');
+const argv = process.argv;
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}/`;
+
+request(url, function(err, res, body) {
+    const data = JSON.parse(body);
+    const chars = data.characters;
+
+    for (let i = 0; i < chars.length; i++) {
+        request(chars[i], function(err, res, body) {
+            const charData = JSON.parse(body);
+            console.log(charData.name);
+        });
+    }
+});
